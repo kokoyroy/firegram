1d0a71db794bba4cf700265ea6a27eeb44b67319
kokoyroy <kokoyroy@gmail.com>
2021-01-12 05:15:32 +0200

    minor improvments2

---
+++ secret.js
@@ -0,0 +1,10 @@
+ const secret ={
+    apiKey: "AIzaSyAS6Bq5M4E2to8rS13LYmd06BIvOb8ElUg",
+    authDomain: "kokoyroy-firegram.firebaseapp.com",
+    projectId: "kokoyroy-firegram",
+    storageBucket: "kokoyroy-firegram.appspot.com",
+    messagingSenderId: "778241594607",
+    appId: "1:778241594607:web:767ce2b3ebf2179c68371a"
+}
+
+export default secret;
